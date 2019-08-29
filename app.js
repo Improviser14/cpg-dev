37ffb9420c0af3686bfb4c2349f70ddba8b09f01
Improviser14 <pavance40@gmail.com>
2019-08-29 18:40:26 -0400

    add nonProfit page link

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-var dotenv     = require('dotenv').config(),
+var dotenv = require('dotenv').config(),
     express = require('express'),
-    // liveServer = require("live-server"),
-    // particlesJS = require("particles.js"),
-    app     = express(),
+    app = express(),
     bodyParser = require('body-parser'),
     nodemailer = require("nodemailer"),
-    request    = require("request"),
+    request = require("request"),
     cookieParser = require("cookie-parser"),
-    router     = express.Router(),
+    router = express.Router(),
     session = require("express-session"),
-    flash      = require("connect-flash"),
+    flash = require("connect-flash"),
     contactRoutes = require("./routes/contact"),
     serveStatic = require('serve-static');
-    
 
-app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(flash());
-
-//  particlesJS.load('particles-js', '/particles.json', function() {
-//   console.log('callback - particles.js config loaded');
-//  });
-
-
 
 app.set('view engine', 'ejs');
 
@@ -31,25 +25,26 @@ app.use("/", router);
 
 app.use(express.static('public/'));
 
-app.get('/contact', function(req, res){
+app.get('/contact', function (req, res) {
     res.render('contact');
 });
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.render('cpg');
 });
 
-app.get('/pricing', function(req, res){
+app.get('/pricing', function (req, res) {
     res.render('pricing');
 });
 
-app.get('/preferences', function(req, res){
+app.get('/preferences', function (req, res) {
     res.render('preferences');
 });
 
 app.use("/contact", contactRoutes);
- 
+
 app.use(flash());
 
-app.listen(process.env.PORT, process.env.IP);
+// app.listen(process.env.PORT, process.env.IP);
 
+app.listen(8080, '127.0.0.1');
