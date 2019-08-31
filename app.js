015fec36ea65730668961a0faa046246e8125a2a
Improviser14 <pavance40@gmail.com>
2019-08-31 09:06:36 -0400

    comment out localhost

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
-var dotenv = require('dotenv').config(),
-    express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser'),
-    nodemailer = require("nodemailer"),
-    request = require("request"),
-    cookieParser = require("cookie-parser"),
-    router = express.Router(),
-    session = require("express-session"),
-    flash = require("connect-flash"),
-    contactRoutes = require("./routes/contact"),
-    serveStatic = require('serve-static');
+var dotenv = require("dotenv").config(),
+  express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  nodemailer = require("nodemailer"),
+  request = require("request"),
+  cookieParser = require("cookie-parser"),
+  router = express.Router(),
+  session = require("express-session"),
+  flash = require("connect-flash"),
+  contactRoutes = require("./routes/contact"),
+  serveStatic = require("serve-static");
 
-
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(flash());
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use("/", router);
 
-app.use(express.static('public/'));
+app.use(express.static("public/"));
 
-app.get('/contact', function (req, res) {
-    res.render('contact');
+app.get("/contact", function(req, res) {
+  res.render("contact");
 });
 
-app.get('/', function (req, res) {
-    res.render('cpg');
+app.get("/", function(req, res) {
+  res.render("cpg");
 });
 
-app.get('/pricing', function (req, res) {
-    res.render('pricing');
+app.get("/pricing", function(req, res) {
+  res.render("pricing");
 });
 
-app.get('/preferences', function (req, res) {
-    res.render('preferences');
+app.get("/preferences", function(req, res) {
+  res.render("preferences");
 });
 
 app.use("/contact", contactRoutes);
@@ -47,4 +48,4 @@ app.use(flash());
 
 app.listen(process.env.PORT, process.env.IP);
 
-app.listen(8080, '127.0.0.1');
+// app.listen(8080, '127.0.0.1');
