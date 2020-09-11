2de6766a745bd560a640dbf2c7c5d8ad118a2870
Improviser14 <pavance40@gmail.com>
2020-09-10 21:35:53 -0400

    enable ssl environment

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ var dotenv = require("dotenv").config(),
   contactRoutes = require("./routes/contact"),
   serveStatic = require("serve-static");
 
+//ssl must be configured on the application level --here
+//uncomment this block when deploying see code at the bottom of this file
+if (process.env.ENVIRONMENT === "prod") {
+  app.use(function(req, res, next) {
+    if (req.get("X-Forwarded-Proto") !== "https") {
+      res.redirect("https://" + req.get("Host") + req.url);
+    } else next();
+  });
+}
+
 app.use(
   bodyParser.urlencoded({
     extended: false
@@ -46,6 +56,14 @@ app.use("/contact", contactRoutes);
 
 app.use(flash());
 
-app.listen(process.env.PORT, process.env.IP);
+if (process.env.ENVIRONMENT === "prod") {
+  // sets port 8080 to default or unless otherwise specified in the environment
+  app.set("port", process.env.PORT || 80);
+  app.listen(app.get("port"));
+} else {
+  app.listen(8080, "127.0.0.1");
+}
 
-app.listen(8080, "127.0.0.1");
+// app.listen(process.env.PORT, process.env.IP);
+
+// app.listen(8080, "127.0.0.1");
